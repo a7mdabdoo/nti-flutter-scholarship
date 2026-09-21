3b1f970ea9c18607bed15cad482ec004632c8d97
a7mdabdoo <a7md.m.3bdo@gmail.com>
2026-09-21 16:09:40 +0300

    refactor(day01): keep only hello world in main.dart and remove human.dart

--- day01/main.dart
+++ day01/main.dart
@@ -1,10 +1,4 @@
-import 'human.dart';
 void main(){
   print("Hello World");
-  Human ahmed = Human(name:"ahmed",age: 20,height: 120);
-  print(ahmed.name);
-  print(ahmed.age);
-  print(ahmed.height);
-  ahmed.numberOfHands=3;
-  print (ahmed.numberOfHands);
+
 }
